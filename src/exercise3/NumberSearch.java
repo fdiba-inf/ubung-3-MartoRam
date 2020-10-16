1efd5a8469965a8d51bf0d6f9dd2d1655578c181
MartoRam <mmmladenov1@gmail.com>
2020-10-16 19:25:19 +0300

    da

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -5,18 +5,23 @@ package exercise3;
 public class NumberSearch {
     public static void main(String[] args) {
         int counter=0;
-        int counter2=0;
+
         for (int i=101;i<=1000;i++)
         {
-            if (i%30==0)
+            if (i%5==0&& i%6==0)
             {
                 counter++;
-                System.out.print(i+" ");
-                if (counter==10)
+
+                if (counter==10 ||counter==20)
+
 
                 {
                     System.out.println(i+" ");
-                    counter=0;
+
+                }
+                else
+                {
+                    System.out.print(i+" ");
                 }
 
             }
